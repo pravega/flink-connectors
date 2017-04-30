66eeffbdd57c018e6eb6ed389f14e2af1674c76e
Suri <suresh.krishnappa@emc.com>
2017-04-30 21:06:59 +0530

    Initial structure for the connectors

---
+++ README.md
@@ -0,0 +1,20 @@
+# pravega-connectors
+
+Sample applications for Pravega.
+
+## Getting Started
+### Building Pravega
+
+Install the Pravega client libraries to your local Maven repository:
+```
+$./gradlew publishMavenPublicationToMavenLocal
+```
+
+### Building the Connector
+Use the built-in gradle wrapper to build the connector.
+```
+$ ./gradlew build
+...
+BUILD SUCCESSFUL
+```
+
---
+++ src/test/java/io/pravega/connectors/flink/utils/SetupUtils.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2017 Dell Inc., or its subsidiaries.
+ */
+package io.pravega.connectors.flink.utils;
+
+import io.pravega.ClientFactory;
+import io.pravega.ReaderGroupManager;
+import io.pravega.StreamManager;
+import io.pravega.local.InProcPravegaCluster;
+import io.pravega.stream.EventStreamReader;
+import io.pravega.stream.EventStreamWriter;
+import io.pravega.stream.EventWriterConfig;
+import io.pravega.stream.ReaderConfig;
+import io.pravega.stream.ReaderGroupConfig;
+import io.pravega.stream.ScalingPolicy;
+import io.pravega.stream.StreamConfiguration;
+import io.pravega.test.common.TestUtils;
+import com.google.common.base.Preconditions;
+import lombok.Cleanup;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.net.URI;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Utility functions for creating the test setup.
+ */
+@Slf4j
+@NotThreadSafe
+public final class SetupUtils {
+    // The pravega cluster.
+    private InProcPravegaCluster inProcPravegaCluster = null;
+
+    // Manage the state of the class.
+    private final AtomicBoolean started = new AtomicBoolean(false);
+
+    // The test Scope name.
+    @Getter
+    private final String scope = "scope";
+
+    /**
+     * Start all pravega related services required for the test deployment.
+     *
+     * @throws Exception on any errors.
+     */
+    public void startAllServices() throws Exception {
+        if (!this.started.compareAndSet(false, true)) {
+            log.warn("Services already started, not attempting to start again");
+            return;
+        }
+
+        int zkPort = TestUtils.getAvailableListenPort();
+        int controllerPort = TestUtils.getAvailableListenPort();
+        int hostPort = TestUtils.getAvailableListenPort();
+        this.inProcPravegaCluster = InProcPravegaCluster.builder()
+                .isInProcZK(true)
+                .zkUrl("localhost:" + zkPort)
+                .zkPort(zkPort)
+                .isInMemStorage(true)
+                .isInProcController(true)
+                .controllerCount(1)
+                .isInProcSegmentStore(true)
+                .segmentStoreCount(1)
+                .containerCount(4)
+                .build();
+        this.inProcPravegaCluster.setControllerPorts(new int[]{controllerPort});
+        this.inProcPravegaCluster.setSegmentStorePorts(new int[]{hostPort});
+        this.inProcPravegaCluster.start();
+        log.info("Initialized Pravega Cluster");
+    }
+
+    /**
+     * Stop the pravega cluster and release all resources.
+     *
+     * @throws Exception on any errors.
+     */
+    public void stopAllServices() throws Exception {
+        if (!this.started.compareAndSet(true, false)) {
+            log.warn("Services not yet started or already stopped, not attempting to stop");
+            return;
+        }
+
+        this.inProcPravegaCluster.close();
+    }
+
+    /**
+     * Fetch the controller endpoint for this cluster.
+     *
+     * @return URI The controller endpoint to connect to this cluster.
+     */
+    public URI getControllerUri() {
+        return URI.create("tcp://" + this.inProcPravegaCluster.getControllerURI());
+    }
+
+    /**
+     * Create the test stream.
+     *
+     * @param streamName     Name of the test stream.
+     * @param numSegments    Number of segments to be created for this stream.
+     *
+     * @throws Exception on any errors.
+     */
+    public void createTestStream(final String streamName, final int numSegments)
+            throws Exception {
+        Preconditions.checkState(this.started.get(), "Services not yet started");
+        Preconditions.checkNotNull(streamName);
+        Preconditions.checkArgument(numSegments > 0);
+
+        @Cleanup
+        StreamManager streamManager = StreamManager.create(getControllerUri());
+        streamManager.createScope(this.scope);
+        streamManager.createStream(this.scope, streamName,
+                StreamConfiguration.builder()
+                        .scope(this.scope)
+                        .streamName(streamName)
+                        .scalingPolicy(ScalingPolicy.fixed(numSegments))
+                        .build());
+        log.info("Created stream: " + streamName);
+    }
+
+    /**
+     * Create a stream writer for writing Integer events.
+     *
+     * @param streamName    Name of the test stream.
+     *
+     * @return Stream writer instance.
+     */
+    public EventStreamWriter<Integer> getIntegerWriter(final String streamName) {
+        Preconditions.checkState(this.started.get(), "Services not yet started");
+        Preconditions.checkNotNull(streamName);
+
+        ClientFactory clientFactory = ClientFactory.withScope(this.scope, getControllerUri());
+        return clientFactory.createEventWriter(
+                streamName,
+                new IntegerSerializer(),
+                EventWriterConfig.builder().build());
+    }
+
+    /**
+     * Create a stream reader for reading Integer events.
+     *
+     * @param streamName    Name of the test stream.
+     *
+     * @return Stream reader instance.
+     */
+    public EventStreamReader<Integer> getIntegerReader(final String streamName) {
+        Preconditions.checkState(this.started.get(), "Services not yet started");
+        Preconditions.checkNotNull(streamName);
+
+        ReaderGroupManager readerGroupManager = ReaderGroupManager.withScope(this.scope, getControllerUri());
+        final String readerGroup = "testReaderGroup" + this.scope + streamName;
+        readerGroupManager.createReaderGroup(
+                readerGroup,
+                ReaderGroupConfig.builder().startingTime(0).build(),
+                Collections.singleton(streamName));
+
+        ClientFactory clientFactory = ClientFactory.withScope(this.scope, getControllerUri());
+        final String readerGroupId = UUID.randomUUID().toString();
+        return clientFactory.createReader(
+                readerGroupId,
+                readerGroup,
+                new IntegerSerializer(),
+                ReaderConfig.builder().build());
+    }
+}
