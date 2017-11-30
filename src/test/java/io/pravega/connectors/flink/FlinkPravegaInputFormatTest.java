710f2833ec74e660daf6fde21c1c0f25b4acba11
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2017-11-30 19:32:17 +0800

    Reset reader group on every execution attempt in FlinkPravegaInputFormat

--- src/test/java/io/pravega/connectors/flink/FlinkPravegaInputFormatTest.java
+++ src/test/java/io/pravega/connectors/flink/FlinkPravegaInputFormatTest.java
@@ -15,6 +15,8 @@ import io.pravega.connectors.flink.utils.SetupUtils;
 import io.pravega.connectors.flink.utils.ThrottledIntegerWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
@@ -95,6 +97,52 @@ public class FlinkPravegaInputFormatTest extends StreamingMultipleProgramsTestBa
         }
     }
 
+    @Test
+    public void testBatchInputWithFailure() throws Exception {
+        final int numElements = 100;
+
+        // set up the stream
+        final String streamName = RandomStringUtils.randomAlphabetic(20);
+        SETUP_UTILS.createTestStream(streamName, 3);
+
+        try (
+                final EventStreamWriter<Integer> eventWriter = SETUP_UTILS.getIntegerWriter(streamName);
+
+                // create the producer that writes to the stream
+                final ThrottledIntegerWriter producer = new ThrottledIntegerWriter(
+                        eventWriter,
+                        numElements,
+                        numElements + 1, // no need to block writer for a batch test
+                        0
+                )
+        ) {
+            // write batch input
+            producer.start();
+            producer.sync();
+
+            final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+            env.setRestartStrategy(RestartStrategies.fixedDelayRestart(1, 1000L));
+            env.setParallelism(3);
+
+            // simple pipeline that reads from Pravega and collects the events
+            List<Integer> integers = env.createInput(
+                    new FlinkPravegaInputFormat<>(
+                            SETUP_UTILS.getControllerUri(),
+                            SETUP_UTILS.getScope(),
+                            Collections.singleton(streamName),
+                            0,
+                            new IntDeserializer()),
+                    BasicTypeInfo.INT_TYPE_INFO
+            ).map(new FailOnceMapper(numElements / 2)).collect();
+
+            // verify that the job did fail, and all events were still read
+            Assert.assertTrue(FailOnceMapper.hasFailed());
+            Assert.assertEquals(numElements, integers.size());
+
+            FailOnceMapper.reset();
+        }
+    }
+
     private static class IntDeserializer extends AbstractDeserializationSchema<Integer> {
 
         @Override
@@ -105,6 +153,35 @@ public class FlinkPravegaInputFormatTest extends StreamingMultipleProgramsTestBa
         @Override
         public boolean isEndOfStream(Integer nextElement) {
             return false;
+        }
+    }
+
+    private static class FailOnceMapper extends RichMapFunction<Integer, Integer> {
+
+        private static boolean failedOnce;
+
+        private final int failCount;
+
+        static void reset() {
+            failedOnce = false;
+        }
+
+        static boolean hasFailed() {
+            return failedOnce;
+        }
+
+        FailOnceMapper(int failCount) {
+            this.failCount = failCount;
+        }
+
+        @Override
+        public Integer map(Integer value) throws Exception {
+            if (!failedOnce && value.equals(failCount)) {
+                failedOnce = true;
+                throw new RuntimeException("Artificial failure");
+            }
+
+            return value;
         }
     }
 }
