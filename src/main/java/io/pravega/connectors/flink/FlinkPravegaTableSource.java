7029cc6d05069fdec534009616b315e179c991f6
Vijay Srinivasaraghavan <vijayaraghavan.srinivasaraghavan@emc.com>
2017-12-07 14:01:06 -0800

    [Issue 85] Updated build scripts to support Flink 1.4.0

--- src/main/java/io/pravega/connectors/flink/FlinkPravegaTableSource.java
+++ src/main/java/io/pravega/connectors/flink/FlinkPravegaTableSource.java
@@ -16,6 +16,7 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.util.serialization.DeserializationSchema;
+import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.sources.StreamTableSource;
 import org.apache.flink.types.Row;
 
@@ -84,6 +85,11 @@ public class FlinkPravegaTableSource implements StreamTableSource<Row> {
     @Override
     public TypeInformation<Row> getReturnType() {
         return typeInfo;
+    }
+
+    @Override
+    public TableSchema getTableSchema() {
+        return TableSchema.fromTypeInfo(typeInfo);
     }
 
     /**
