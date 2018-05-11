6a2d2a413968b0a07eb18a947c58528f06c59020
Vijay Srinivasaraghavan <vijayaraghavan.srinivasaraghavan@emc.com>
2018-05-10 17:43:29 -0700

    [issue-119] added jcenter publishing configurations * replaced shell script with gradle-git plugin to derive snapshot version label * removed developers section from pom configuration
    Signed-off-by: Vijay Srinivasaraghavan <vijayaraghavan.srinivasaraghavan@emc.com>

--- version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-CONNECTOR_VERSION=$(grep 'connectorVersion=' gradle.properties)
-BASE_VERSION=$(echo ${CONNECTOR_VERSION}  | sed 's/connectorVersion=//' | sed 's/-.*//')
-VERSION="${BASE_VERSION}-$(git rev-list HEAD --count).$(git rev-parse --short HEAD)-$(date +%s)"
-case "${CONNECTOR_VERSION}" in
-*SNAPSHOT*) echo "${VERSION}-SNAPSHOT" ;;
-*         ) echo "${VERSION}" ;;
-esac
