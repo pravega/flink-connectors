773be391c5ff63689bf5cbbbd772ad90d3ce1429
Vijay Srinivasaraghavan <vijikarthi@yahoo.com>
2019-04-10 10:59:38 -0700

    [issue-216] Update expired Pravega certs and ensure system test works (#217)
    * Removes old certs and replaced it with Pravega standalone certs
    * Fixes failing test cases specific to TLS integration
    * Updates Pravega to latest snapshot version
    
    Signed-off-by: Vijay Srinivasaraghavan <vijayaraghavan.srinivasaraghavan@emc.com>

--- src/test/java/io/pravega/connectors/flink/FlinkTableITCase.java
+++ src/test/java/io/pravega/connectors/flink/FlinkTableITCase.java
@@ -241,9 +241,7 @@ public class FlinkTableITCase {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment().setParallelism(1);
         StreamTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
 
-        PravegaConfig pravegaConfig = PravegaConfig.fromDefaults()
-                .withControllerURI(setupUtils.getControllerUri())
-                .withDefaultScope(setupUtils.getScope());
+        PravegaConfig pravegaConfig = setupUtils.getPravegaConfig();
 
         Pravega pravega = new Pravega();
         pravega.tableSinkWriterBuilder()
@@ -309,9 +307,7 @@ public class FlinkTableITCase {
         env.setParallelism(1);
         BatchTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
 
-        PravegaConfig pravegaConfig = PravegaConfig.fromDefaults()
-                .withControllerURI(setupUtils.getControllerUri())
-                .withDefaultScope(setupUtils.getScope());
+        PravegaConfig pravegaConfig = setupUtils.getPravegaConfig();
 
         Pravega pravega = new Pravega();
         pravega.tableSinkWriterBuilder()
