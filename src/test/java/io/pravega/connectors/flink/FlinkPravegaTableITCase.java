31abfea536fd016880d551922451954d35f19e86
AlexanderZhao1 <Alexander_Zhao@dell.com>
2020-02-14 14:22:28 +0800

    [issue-329] Update-for-flink1.10-release
    Signed-off-by: AlexanderZhao1 <Alexander_Zhao@dell.com>

--- src/test/java/io/pravega/connectors/flink/FlinkPravegaTableITCase.java
+++ src/test/java/io/pravega/connectors/flink/FlinkPravegaTableITCase.java
@@ -28,6 +28,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.api.java.BatchTableEnvironment;
