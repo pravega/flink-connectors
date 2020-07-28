2c916a8bca7de9f1aaee53d98e54fe31f3a9896f
Guangfeng Xu <Guangfeng.Xu@dell.com>
2020-07-28 13:31:54 +0800

    [issue-399] Fail the reader when RuntimeException are caught (#400)
    Signed-off-by: Guangfeng Xu <Guangfeng.Xu@dell.com>

--- src/main/java/io/pravega/connectors/flink/FlinkPravegaReader.java
+++ src/main/java/io/pravega/connectors/flink/FlinkPravegaReader.java
@@ -317,6 +317,10 @@ public class FlinkPravegaReader<T>
                 }
             }
         }
+        catch (RuntimeException e) {
+            log.error("Exception occurred while creating a Pravega EventStreamReader to read events", e);
+            throw e;
+        }
     }
 
     @Override
