cbfd3493b21320de965ddf6f54427bab7e646258
Youmin Han <Youmin.Han@dell.com>
2020-09-29 22:38:41 -0400

    [issue-413] Ensure the reader name below the length limit (#414)
    Signed-off-by: Youmin Han <Youmin.Han@dell.com>

---
+++ src/test/java/io/pravega/connectors/flink/util/FlinkPravegaUtilsTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.connectors.flink.util;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class FlinkPravegaUtilsTest {
+
+    @Test
+    public void testGetReaderName() {
+        String testStr = new String(new char[256]).replace('\0', 'a');
+        assertTrue(FlinkPravegaUtils.getReaderName(testStr, 1, 1).length() < 256);
+    }
+}
