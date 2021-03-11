c2a843ee9885050d27f93c5f0c8cb22e969cae73
Claudio Fahey <claudio.fahey@dell.com>
2021-03-11 01:11:08 +0000

    Add sidebars.js

---
+++ documentation/src/docs/sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mainSidebar: [
+        'index',
+        'getting-started',
+        'quickstart',
+        {
+            'Features': [
+                'streaming',
+                'batch',
+                'table-api',
+            ],
+        },
+        'metrics',
+        'configurations',
+        'serialization',
+    ]
+};
