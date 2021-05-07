44aba0c6be66b3aba51c7b2afaed7b241e26aee2
fyang86 <48351067+fyang86@users.noreply.github.com>
2021-05-07 09:42:35 +0800

    [Issue-424] Implement Pravega Catalog (#498)
    - Add avro support
    Signed-off-by: Fan, Yang <fan.yang5@emc.com>

---
+++ src/main/java/io/pravega/connectors/flink/table/catalog/pravega/factories/PravegaCatalogFactory.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.pravega.connectors.flink.table.catalog.pravega.factories;
+
+import io.pravega.connectors.flink.table.catalog.pravega.PravegaCatalog;
+import io.pravega.connectors.flink.table.catalog.pravega.descriptors.PravegaCatalogValidator;
+import org.apache.flink.table.catalog.Catalog;
+import org.apache.flink.table.descriptors.DescriptorProperties;
+import org.apache.flink.table.factories.CatalogFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.pravega.connectors.flink.table.catalog.pravega.descriptors.PravegaCatalogValidator.*;
+import static org.apache.flink.table.descriptors.CatalogDescriptorValidator.CATALOG_DEFAULT_DATABASE;
+
+public class PravegaCatalogFactory implements CatalogFactory {
+
+    @Override
+    public Map<String, String> requiredContext() {
+        HashMap<String, String> context = new HashMap<>();
+        context.put(CATALOG_TYPE, CATALOG_TYPE_VALUE_PRAVEGA);
+        context.put(CATALOG_PROPERTY_VERSION, "1"); // backwards compatibility
+        return context;
+    }
+
+    @Override
+    public List<String> supportedProperties() {
+        List<String> props = new ArrayList<>();
+
+        props.add(CATALOG_DEFAULT_DATABASE);
+
+        props.add(CATALOG_CONTROLLER_URI);
+        props.add(CATALOG_SCHEMA_REGISTRY_URI);
+
+        return props;
+    }
+
+    @Override
+    public Catalog createCatalog(String name, Map<String, String> properties) {
+        final DescriptorProperties dp = getValidateProperties(properties);
+
+        return new PravegaCatalog(
+                name,
+                dp.getString(CATALOG_DEFAULT_DATABASE),
+                dp.getString(CATALOG_CONTROLLER_URI),
+                dp.getString(CATALOG_SCHEMA_REGISTRY_URI));
+    }
+
+    private DescriptorProperties getValidateProperties(Map<String, String> properties) {
+        final DescriptorProperties dp = new DescriptorProperties();
+        dp.putProperties(properties);
+
+        new PravegaCatalogValidator().validate(dp);
+        return dp;
+    }
+}
---
+++ src/main/java/io/pravega/connectors/flink/table/catalog/pravega/util/PravegaSchemaUtils.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.pravega.connectors.flink.table.catalog.pravega.util;
+
+import io.pravega.schemaregistry.contract.data.SchemaInfo;
+import io.pravega.schemaregistry.contract.data.SerializationFormat;
+import io.pravega.schemaregistry.serializer.avro.schemas.AvroSchema;
+import io.pravega.schemaregistry.serializer.json.schemas.JSONSchema;
+import org.apache.avro.Schema;
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.formats.avro.typeutils.AvroSchemaConverter;
+import org.apache.flink.formats.json.JsonRowSchemaConverter;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.utils.DataTypeUtils;
+import org.apache.flink.table.types.utils.TypeConversions;
+
+@Internal
+public class PravegaSchemaUtils {
+
+    private PravegaSchemaUtils() {
+        // private
+    }
+
+    public static TableSchema schemaInfoToTableSchema(SchemaInfo schemaInfo) {
+
+        SerializationFormat format = schemaInfo.getSerializationFormat();
+        String schemaString;
+        DataType dataType;
+
+        switch (format) {
+            case Json:
+                ObjectMapper objectMapper = new ObjectMapper();
+                JSONSchema jsonSchema = JSONSchema.from(schemaInfo);
+
+                try {
+                    schemaString = objectMapper.writeValueAsString(jsonSchema.getSchema());
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException("Failed to write message schema.", e);
+                }
+
+                dataType = TypeConversions.fromLegacyInfoToDataType(JsonRowSchemaConverter.convert(schemaString));
+                break;
+            case Avro:
+                AvroSchema avroSchema = AvroSchema.from(schemaInfo);
+
+                schemaString = avroSchema.getSchema().toString();
+                dataType = TypeConversions.fromLegacyInfoToDataType(AvroSchemaConverter.convertToTypeInfo(schemaString));
+                break;
+
+            default:
+                throw new NotImplementedException("Not supporting serialization format");
+        }
+
+        return DataTypeUtils.expandCompositeTypeToSchema(dataType);
+    }
+
+    public static SchemaInfo tableSchemaToSchemaInfo(TableSchema tableSchema) {
+        // only support avro format for now
+        Schema schema = AvroSchemaConverter.convertToSchema(tableSchema.toRowDataType().getLogicalType());
+        AvroSchema avroSchema = AvroSchema.of(schema);
+        return avroSchema.getSchemaInfo();
+    }
+}
