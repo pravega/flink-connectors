5507b16071064740cc1fc180f74dfd566f635851
Fan, Yang <fan.yang5@emc.com>
2021-08-09 13:54:48 +0800

    addressing comments
    Signed-off-by: Fan, Yang <fan.yang5@emc.com>

--- src/main/java/io/pravega/connectors/flink/source/enumerator/PravegaSplitEnumerator.java
+++ src/main/java/io/pravega/connectors/flink/source/enumerator/PravegaSplitEnumerator.java
@@ -1,11 +1,17 @@
 /**
- * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ * Copyright Pravega Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.pravega.connectors.flink.source.enumerator;
@@ -63,7 +69,7 @@ public class PravegaSplitEnumerator implements SplitEnumerator<PravegaSplit, Che
     // Pravega splits list.
     private final List<PravegaSplit> splits;
 
-    // Pravega checkpoint for enymerator.
+    // Pravega checkpoint for enumerator.
     private Checkpoint checkpoint;
 
     // Flag to indicate whether it's already in the recovering process
@@ -178,8 +184,8 @@ public class PravegaSplitEnumerator implements SplitEnumerator<PravegaSplit, Che
         readerGroupManager.close();
     }
 
-    // This method is called when it tries to "regionally" recover from a failure to assign unassigned splits. We will
-    // throw an intentional exception here to trigger a global recovery since Pravega since Pravega
+    // This method is called when there is an attempt to locally recover from a failure to assign unassigned splits.
+    // It throws an intentional exception here to trigger a global recovery since Pravega since Pravega
     // doesn't have a mechanism to recover from partial failure. This will shutdown the reader group,
     // recreate it and recover it from the latest checkpoint.
     @Override
