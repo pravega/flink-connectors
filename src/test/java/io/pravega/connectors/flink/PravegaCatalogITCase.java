479970327422df7ba5c29ebee4fd7171b2eff2dc
root <garima.malhotra@dell.com>
2024-02-04 15:44:27 +0000

    [KARMAN-11003] assertThat fix
    Signed-off-by: root <garima.malhotra@dell.com>

--- src/test/java/io/pravega/connectors/flink/PravegaCatalogITCase.java
+++ src/test/java/io/pravega/connectors/flink/PravegaCatalogITCase.java
@@ -135,7 +135,7 @@ public class PravegaCatalogITCase {
         assertThat(actualCatalog instanceof PravegaCatalog).isTrue();
         assertThat(((PravegaCatalog) actualCatalog).getName()).isEqualTo(CATALOG.getName());
         assertThat(((PravegaCatalog) actualCatalog).getDefaultDatabase()).isEqualTo(CATALOG.getDefaultDatabase());
-        org.assertj.core.api.Assertions.assertThat((Map) Whitebox.getInternalState(actualCatalog, "properties"))
+        assertThat((Map) Whitebox.getInternalState(actualCatalog, "properties"))
                 .isEqualTo(Whitebox.getInternalState(CATALOG, "properties"));
     }
 
